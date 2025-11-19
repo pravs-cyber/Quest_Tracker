2fe6c8b0cbdd21404bfe7e83e8a846ec6fe96c12
pravs-cyber <pravallika.sahas@gmail.com>
2025-11-19 18:13:08 +0530

    Update geminiService.ts

--- src/services/geminiService.ts
+++ src/services/geminiService.ts
@@ -1,5 +1,5 @@
 
-import { GoogleGenAI, Type, Schema } from "@google/genai";
+import { GoogleGenAI, Type, Schema } from "@google/generative-ai";
 import { BigGoal, GoalStep } from "../types";
 
 const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_API_KEY });
